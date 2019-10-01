696fb8e2ed54c48a11dfcb612905bba683fe0e50
allisonbusse <allisonbusse@gmail.com>
2019-10-01 14:40:55 -0700

    setup and first routes

---
+++ e2e/match-helpers.js
@@ -0,0 +1,9 @@
+
+const mongoId = /^[a-f\d]{24}$/i;
+
+const matchMongoId = '_id: expect.stringMatching(mongoId)';
+
+module.exports = {
+  mongoId,
+  matchMongoId
+};
---
+++ e2e/setup.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const connect = require('../lib/connect');
+const MONGODB_URI = global.__MONGO_URI__;
+const mongoose = require('mongoose');
+
+beforeAll(() => {
+  return connect(MONGODB_URI, { log: false });
+});
+
+afterAll(() => {
+  return mongoose.connection.close();
+});
